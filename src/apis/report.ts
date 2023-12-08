30e4a687e417fa04f6a60af221d14e2164597454
anttiey <hanuuny@gmail.com>
2023-12-08 20:58:43 +0900

    :sparkles: 신고 API 추가

--- src/apis/report.ts
+++ src/apis/report.ts
@@ -1,6 +1,15 @@
 import request from './request';
 import { handleApiError } from './errorHandling';
 
+export const UserListAllApi = async () => {
+  try {
+    const response = await request.get(`report/get_all_user`);
+    return response.data;
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
+
 export const ReportListAllApi = async () => {
   try {
     const response = await request.get(`report/get_all_reports`);
@@ -13,6 +22,15 @@ export const ReportListAllApi = async () => {
 export const ReportListSuspiciousApi = async () => {
   try {
     const response = await request.get(`/report/get_suspicious_users`);
+    return response.data;
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
+
+export const BanUserApi = async (id: string) => {
+  try {
+    const response = await request.post(`/report/ban_user?memberId=${id}`);
     return response.data;
   } catch (error) {
     return handleApiError(error);
