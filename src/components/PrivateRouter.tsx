1f330df3aeb17fb56e77842a8d091ccbffb177c3
anttiey <hanuuny@gmail.com>
2023-12-08 22:59:33 +0900

    :bug: 라우팅 오류 해결

--- src/components/PrivateRouter.tsx
+++ src/components/PrivateRouter.tsx
@@ -1,7 +1,11 @@
 import { Navigate } from 'react-router-dom';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const PrivateRouter = (element: any) => {
+type PrivateRouterProps = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  element: any;
+};
+
+const PrivateRouter: React.FC<PrivateRouterProps> = ({ element }) => {
   if (localStorage.getItem('token') === null) {
     alert('로그인이 필요한 기능입니다.');
   }
